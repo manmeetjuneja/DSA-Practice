245512aa61b5b41119a2581841028add9528f2b7
manmeetjuneja <49336313+manmeetjuneja@users.noreply.github.com>
2025-05-04 16:44:18 +0530

    [Manmeet] Recursive approach

--- src/main/java/org/example/Arrays/LargestElement.java
+++ src/main/java/org/example/Arrays/LargestElement.java
@@ -3,6 +3,7 @@ package org.example.Arrays;
 import java.util.Arrays;
 
 import static org.example.Constants.Constants.BRUTE_FORCE;
+import static org.example.Constants.Constants.RECURSIVE_APPROACH;
 
 public class LargestElement {
 
@@ -12,7 +13,24 @@ public class LargestElement {
         int index = arr.length;
         Arrays.sort(arr);
         System.out.println("Largest Element is : "+arr[index-1]);
+        //Time Complexity: O(N*log(N))
+        //Space Complexity: O(n)
+    }
 
+    public static void recursiveApproach(int[] arr) {
+        // Assuming  max as first element is array
+        // iterating the array till length
+        // changing the max if found more the assumed.
+        int max = arr[0];
+        int size = arr.length;
+        for (int i = 0; i < size; i++) {
+            if (arr[i]>max){
+                max=arr[i];
+            }
+        }
+        System.out.println("Largest Element is : "+max);
+//        Time Complexity: O(N)
+//        Space Complexity: O(1)
     }
 
     public static void main(String[] args) {
@@ -21,5 +39,8 @@ public class LargestElement {
         System.out.println(BRUTE_FORCE);
         bruteForce(arr1);
         bruteForce(arr2);
+        System.out.println(RECURSIVE_APPROACH);
+        recursiveApproach(arr1);
+        recursiveApproach(arr2);
     }
 }
--- src/main/java/org/example/Constants/Constants.java
+++ src/main/java/org/example/Constants/Constants.java
@@ -2,4 +2,5 @@ package org.example.Constants;
 
 public class Constants {
     public static final String BRUTE_FORCE="Brute Force Approach";
+    public static final String RECURSIVE_APPROACH="Recursive Approach";
 }
