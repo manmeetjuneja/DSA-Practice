e12666f4da92033b78a9616e2168b8bf28cd9580
manmeetjuneja <49336313+manmeetjuneja@users.noreply.github.com>
2025-05-06 23:32:46 +0530

    [Manmeet] Better approach Test

--- src/main/java/org/example/Arrays/SecondSmallest_SecondLargest.java
+++ src/main/java/org/example/Arrays/SecondSmallest_SecondLargest.java
@@ -1,4 +1,40 @@
 package org.example.Arrays;
 
+import java.util.Arrays;
+
+import static org.example.Constants.Constants.BRUTE_FORCE;
+
 public class SecondSmallest_SecondLargest {
+    public static void bruteForce(int[] arr) {
+        // Sorting the array in ascending order
+        // returnin the elements in order
+        // of not found will return -1
+
+        int size = arr.length;
+        if (size == 0 || size == 1) {
+            System.out.println("-1");
+        } else {
+            Arrays.sort(arr);
+            System.out.println("Second Small : " + arr[1]);
+            System.out.println("Second Largest : " + arr[size - 2]);
+        }
+    }
+
+    public static void recursiveApproach() {
+
+    }
+
+    public static void betterApproach() {
+
+    }
+
+    public static void optimalApproach() {
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 4, 6, 7, 5};
+        System.out.println(BRUTE_FORCE);
+        bruteForce(arr);
+    }
 }
